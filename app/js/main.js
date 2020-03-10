0c1beca020e11b9e914086d6744074ef433e1c8a
ellavrubel <webella@protonmail.com>
2020-03-10 19:31:43 +0300

    nav/logo

--- app/js/main.js
+++ app/js/main.js
@@ -4,10 +4,18 @@
             infinity: true,
             fade: true,
             prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg">',
-            nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg">'
+            nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg">',
+            asNavFor: '.slider-dots'
+
+        });
+
+        $('.slider-dots').slick({
+            asNavFor: '.header-slider',
+            slidesToShow: 4,
+            slidesToScroll: 4
+        });
 
 
-        })
 
 
 
