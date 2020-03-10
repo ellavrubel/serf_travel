665f39efb5366d4e19f82fb39e28125626bfb12c
ellavrubel <webella@protonmail.com>
2020-03-10 11:28:33 +0300

    first block in progress

---
+++ app/js/main.js
@@ -0,0 +1,29 @@
+    $(function () {
+
+        $('.header__slider').slick({
+            infinity: true,
+            fade: true,
+            prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg">',
+            nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg">'
+
+
+        })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    });
