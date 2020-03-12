e4102a20e0a8547fd6ca289e19bde9b33be1ab10
ellavrubel <webella@protonmail.com>
2020-03-12 13:06:23 +0300

    second block fin prog

--- app/js/main.js
+++ app/js/main.js
@@ -16,6 +16,18 @@
         });
 
 
+        $('.serf-slider').slick({
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            prevArrow: '<img class="slider-arrows slider-arrows-left" src="img/arrows-left.svg">',
+            nextArrow: '<img class="slider-arrows slider-arrows-right" src="img/arrows-right.svg">'
+
+
+
+
+        });
+
+
 
 
 
